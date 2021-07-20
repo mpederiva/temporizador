cd505f72fc7f601a118b1f756fde164f2719fff0
Marta Pederiva <80361071+mpederiva@users.noreply.github.com>
2021-07-20 13:00:48 -0300

    Add files via upload

---
+++ contador.js
@@ -0,0 +1,40 @@
+window.addEventListener("load", () => {
+    const botaoI = document.getElementById("botaoI");
+    const botaoP = document.getElementById("botaoP");
+
+    botaoI.addEventListener("click",() => {
+
+        // Tempo, em segundos, que queremos.
+        let sec = 180;
+        
+        const countDiv = document.getElementById("timer");
+        
+        const secpass = () => {
+        
+            let min = Math.floor(sec/60);
+            let segundosRestantes = sec % 60;
+        
+            if (segundosRestantes < 10) {
+                segundosRestantes = "0" + segundosRestantes;
+            }
+        
+            if (min < 10) {
+                min = "0" + min;
+            }
+        
+            // Vai gerar o formato de 02:59
+            countDiv.innerHTML = min + ":" + segundosRestantes;
+        
+            // Condicao final
+            if (sec > 0) {
+                sec = sec - 1;
+            }
+            else {
+                countDiv.innerHTML = "Acabou!";
+            }
+        };
+        
+        const countDown = setInterval(() => secpass(), 1000);
+    
+    });
+})
